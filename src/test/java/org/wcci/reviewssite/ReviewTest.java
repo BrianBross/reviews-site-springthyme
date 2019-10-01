fa97f958ea068832845f64e29a6f88747659606c
Cholliex <ochollieoh@yahoo.com>
2019-10-01 14:56:47 -0400

    Review Test

--- src/test/java/org/wcci/reviewssite/ReviewTest.java
+++ src/test/java/org/wcci/reviewssite/ReviewTest.java
@@ -1,13 +1,24 @@
 package org.wcci.reviewssite;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
 import org.junit.Test;
 
 public class ReviewTest { 
 	
 	@Test
-	public void canCreateReview {
-		
-		
+	public void canCreateReview() {
+		String underTestReviewTitle = "Test Review Title";
+		String underTestProductInfo = "Test Product Title";
+		String underTestUserName = "Test Author";
+		Date underTestReviewDate = new Date();
+		String underTestProductCategory = "Test Category";
+		String underTestReviewBody = "This book was so good it made me wanna slap my momma.";
+		Review underTestReview = new Review(underTestReviewTitle, underTestProductInfo, underTestUserName,
+				underTestReviewDate, underTestProductCategory, underTestReviewBody);
+		assertEquals(underTestReview.getReviewTitle(),underTestReviewTitle);
 	}
 	
 
