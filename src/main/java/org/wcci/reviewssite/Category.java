eca4f28544930a7717aed737b43f736a3ebceced
Brian Bross <brian.bross@gmail.com>
2019-10-08 15:13:03 -0400

    added ID annotation to category and tag

--- src/main/java/org/wcci/reviewssite/Category.java
+++ src/main/java/org/wcci/reviewssite/Category.java
@@ -3,10 +3,16 @@ package org.wcci.reviewssite;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 @Entity
 public class Category {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
 	private String categoryName;
 	@OneToMany (mappedBy = "category")
 	private List<Review> reviews;
--- src/main/java/org/wcci/reviewssite/Tag.java
+++ src/main/java/org/wcci/reviewssite/Tag.java
@@ -3,13 +3,18 @@ package org.wcci.reviewssite;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 
 @Entity
 public class Tag {
 	
-
+		@Id
+		@GeneratedValue
+		private Long id;
+		
 		private String tagName;
 		@ManyToMany
 		private List<Review> reviews;
