51fae8c0f3c04dc371b71646b354a6e3e09544f1
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-10 15:09:31 -0400

    AddReview is broken - Thymeleaf something wrong

--- src/main/java/org/wcci/reviewssite/Category.java
+++ src/main/java/org/wcci/reviewssite/Category.java
@@ -23,4 +23,48 @@ public class Category {
 	public Category(String nameOfCategory) {
 		this.categoryName = nameOfCategory;
 	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", categoryName=" + categoryName + ", reviews=" + reviews + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoryName == null) ? 0 : categoryName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((reviews == null) ? 0 : reviews.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (categoryName == null) {
+			if (other.categoryName != null)
+				return false;
+		} else if (!categoryName.equals(other.categoryName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (reviews == null) {
+			if (other.reviews != null)
+				return false;
+		} else if (!reviews.equals(other.reviews))
+			return false;
+		return true;
+	}
+	
+	
 }
