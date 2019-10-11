57bb7ca5a67a4a6cc1e613f842793b7c6575d8e5
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-11 12:08:10 -0400

    fixing review.html

---
+++ src/main/java/org/wcci/reviewssite/ScratchPad.java
@@ -0,0 +1,68 @@
+package org.wcci.reviewssite;
+
+public class ScratchPad {
+	
+
+
+
+	 <!--          <form class="add-review-form" action="/add" method="POST"> 
+	              
+	                <fieldset>
+	                    <div class="add-review-form__field">
+	                        <label for="bookTitle" class="add-review-form__field__label">Book title being reviewed:</label>
+	                        <input class="add-review-form__field__input" type="text" id="bookTitle"
+	                            name="bookTitle"></input>
+	                    </div>
+	                    
+	                    <div class="add-review-form__field">
+	                        <label for="userName" class="add-review-form__field__label">userName :</label>
+	                        <input class="add-review-form__field__input" type="text" id="userName"
+	                            name="userName"></input>
+	                    </div>
+	                    
+	                    <div class="add-review-form__field">
+	                        <label for="reviewTitle" class="add-review-form__field__label">Enter your review title here  :</label>
+	                        <input class="add-review-form__field__input" type="text" id="reviewTitle"
+	                            name="reviewTitle"></input>
+	                    </div>
+	                    
+	                    <div class="add-review-form__field">
+	                       <label for="categoryName" class="add-review-form__field__label">Select category:</label>
+	                           <select class="add-review-form__field__dropdown" name="categoryName">
+	                               <option th:each="category : ${categories}" th:value="${category.id}"
+	                                   th:text="${category.categoryName}">default</option>
+	                           </select>
+	                    </div>                    
+	                    
+	                    <div class="add-review-form__field">
+	                        <label for="reviewBody" class="add-review-form__field__label">Add your review here:</label>
+	                        <textarea class="add-review-form__field__textarea" id="reviewBody"
+	                            name="reviewBody"></textarea>
+	                    </div>
+	                   
+
+
+
+
+	                   <div class="add-review-form__field"> 
+	                        <label for="review_image" class="add-review-form__field__label">Upload an image
+	                            (optional):</label>
+	                        <input class="add-review-form__field__input" type="text" id="review_image"
+	                            name="review_image"></input>
+	                    </div>
+	                    
+	                    
+
+
+	                    
+	                    <div class="add-review-form__field">
+	                        <input class="add-review-form__field__button primary_action" type="submit" name="submit"
+	                            value="Submit Review">
+	                    </div>
+	                    
+	                </fieldset>
+	            </form>
+	 -->
+
+
+}
