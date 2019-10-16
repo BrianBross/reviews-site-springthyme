7191a1547fb0b4417623e5be728a551b46ae9fae
Diana McIntosh <dotteddice@yahoo.com>
2019-10-16 15:03:55 -0400

    search by review title works but is case sensitive

--- src/main/java/org/wcci/reviewssite/ReviewStorage.java
+++ src/main/java/org/wcci/reviewssite/ReviewStorage.java
@@ -1,5 +1,7 @@
 package org.wcci.reviewssite;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,12 +18,26 @@ public class ReviewStorage {
 		return reviewRepo.findById(id).get();
 	}
 
-	public Iterable<Review> findAllTheReviews() {
-		return reviewRepo.findAllByOrderByIdDesc();
-	}
-	
 	public void addLike(Review reviewToLike) {
 		reviewToLike.setLikes();
 		reviewRepo.save(reviewToLike);
 	}
+
+	public Iterable<Review> findAllTheReviews() {
+		return reviewRepo.findAllByOrderByIdDesc();
+	}
+
+	public Iterable<Review> performReviewTitleSearch(String searchData) {
+
+		System.out.println("We are in the storage method now.");
+		
+		List<Review> returnedReviews = (List<Review>) reviewRepo.findByReviewTitleContains(searchData);
+
+		for (Review review : returnedReviews) {
+			System.out.println("Storage: " + review.getReviewTitle());
+		}
+
+		return reviewRepo.findByReviewTitleContains(searchData);
+	}
+
 }
